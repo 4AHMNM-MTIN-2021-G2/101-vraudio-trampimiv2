ef0c2752fdae33f7aa07ecc81128713bd79e534d
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-06-19 09:43:15 +0200

    Add Animation Entrance

---
+++ Assets/MyGame/Scripts/AnimateDoorScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateDoorScript : MonoBehaviour
+{
+    private Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    public void DoorAnimationOpen()
+    {
+        myAnimator.SetBool("openDoor", true);
+    }
+ }
