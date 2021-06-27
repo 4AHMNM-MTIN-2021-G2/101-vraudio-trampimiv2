cb37784ef6ef9227143848b5eb31510d2fa9295e
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-06-27 13:21:51 +0200

    Maze verziert und collectible Script

--- Assets/MyGame/Scripts/Collectible.cs
+++ Assets/MyGame/Scripts/Collectible.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    public string collectibleColor;
+    public bool isClollected;
+    public GameData gameData;
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("col: " + collision.gameObject.name);
+        gameData.AddCollectible(collectibleColor);
+        Destroy(gameObject);
     }
-
 }
